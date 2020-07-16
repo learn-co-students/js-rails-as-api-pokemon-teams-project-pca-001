7458a52d4be3cc9d74672a24a6ca6ad387fa9233
Liam Tinney <ltinney@powerhrg.com>
2020-07-16 11:05:00 -0400

    major updates

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -1,2 +1,21 @@
 class PokemonsController < ApplicationController
+  def create
+    pokemon = Pokemon.new(pokemon_params)
+    pokemon.nickname = Faker::Name.first_name
+    pokemon.species = Faker::Games::Pokemon.name
+    if pokemon.save
+      render json: pokemon.to_json(
+        except: [:created_at, :udpated_at]
+      )
+      # things went right
+    else
+      # things didn't go correctly.
+    end
+  end
+
+private
+
+  def pokemon_params
+    params.require(:pokemon).permit(:trainer_id)
+  end
 end
